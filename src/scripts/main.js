cfb15b3cd9bca05555b8ba0ceb756312534254dd
jacube1 <jakubfedorczyk@gmail.com>
2025-11-07 22:53:17 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function getSalaryValue(element) {
+  const salaryString = element.dataset.salary;
+  const cleanSalary = salaryString.replace(/\$|,/g, '');
+
+  return Number(cleanSalary);
+}
+
+function sortList(list) {
+  const listItems = Array.from(list.children);
+
+  const itemsToSort = listItems.slice(0, 10);
+
+  itemsToSort.sort((a, b) => {
+    const salaryA = getSalaryValue(a);
+    const salaryB = getSalaryValue(b);
+
+    return salaryB - salaryA;
+  });
+
+  // Dodaj posortowane elementy z powrotem do listy
+  for (const item of itemsToSort) {
+    list.appendChild(item);
+  }
+}
+
+function getEmployees(list) {
+  return Array.from(list.children)
+    .slice(0, 10) // TYLKO 10 pierwszych elementów
+    .map((item) => {
+      return {
+        imię: item.textContent.trim(),
+        stanowisko: item.dataset.position,
+        wynagrodzenie: getSalaryValue(item),
+        wiek: Number(item.dataset.age),
+      };
+    });
+}
+
+// Posortuj listę
+sortList(employeeList);
+
+// Pobierz tablicę pracowników - DODAJ TE LINIĘ
+getEmployees(employeeList);
